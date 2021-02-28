c0e084415f614e3055b74d7805445715449bfc6f
Rafa <sbezerra.rafa@gmail.com>
2021-02-28 11:09:39 -0300

    chore: setting up mongoose,dontev and creating database conecting

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import routes from './routes'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const server = express()
 const port = process.env.PORT || 3000
 
+mongoose.connect(
+  'mongodb+srv://rafa:trecoxd@cluster0.l03hd.mongodb.net/bloggy-api?retryWrites=true&w=majority',
+  { useNewUrlParser: true.valueOf, useUnifiedTopology: true }
+)
+
 server.use(express.json())
+server.use(routes)
 
 server.listen(port, () => {
   console.log(`ei abestado tua api ta rodando http://localhost:${port}`)
