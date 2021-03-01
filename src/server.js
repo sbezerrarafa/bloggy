6cd5debf4e0e0aca2ac91cb252bfe7dc6106f023
Rafa <sbezerra.rafa@gmail.com>
2021-02-28 22:00:31 -0300

    chore: ajuste de alguns erros, de login com o mongodb, tava dando erro, informando que tava login errado

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const server = express()
 const port = process.env.PORT || 3000
 
 mongoose.connect(
-  'mongodb+srv://rafa:trecoxd@cluster0.l03hd.mongodb.net/bloggy-api?retryWrites=true&w=majority',
+  `mongodb+srv://rafa:${process.env.DB_PASSWORD}@cluster0.l03hd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true.valueOf, useUnifiedTopology: true }
 )
 
