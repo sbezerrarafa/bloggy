15bd9e961dd747d7d6c61bc8c3f88477530c72e1
Rafa <sbezerra.rafa@gmail.com>
2021-03-01 21:56:32 -0300

    feat= hasshing password before create user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,25 +1,29 @@
 import User from '../models/User'
 
-export const storeUser = async (request, response) => {
-  const { name, email, password, bio, avatar } = request.body
+class UserController {
+  async store(request, response) {
+    const { name, email, password, bio, avatar } = request.body
 
-  const isUserRegistered = await User.findOne({ email })
+    const isUserRegistered = await User.findOne({ email })
 
-  if (isUserRegistered) {
-    return response
-      .status(409)
-      .json({ error: 'já tem um caba registrado com este e-mail' })
+    if (isUserRegistered) {
+      return response
+        .status(409)
+        .json({ error: 'já tem um caba registrado com este e-mail' })
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      bio,
+      avatar,
+    })
+    let userResult = user.toObject()
+
+    delete userResult['password']
+    return response.status(201).json(userResult)
   }
-
-  const user = await User.create({
-    name,
-    email,
-    password,
-    bio,
-    avatar,
-  })
-  let userResult = user.toObject()
-
-  delete userResult['password']
-  return response.status(201).json(userResult)
 }
+
+export default new UserController()
